0498c3f1dbac213ad731f40f01c5e8b1ad35f1b6
Masoud Moradi <147343920+massi-web-app@users.noreply.github.com>
2025-03-17 14:32:06 +0300

    added route for test controller

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function test()
+    {
+        dd('yes111');
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,18 @@
 <?php
 
 
+use App\Http\Controllers\TestController;
+
 Route::get('/', function () {
     return view('index');
 });
 
 
-Route::get('/test',function(){
-
-
+Route::get('/test', function () {
     return new \Mass\User\Mail\VerifyCodeMail(121212);
 });
+
+Route::get('/hello', [testController::class, 'Test']);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
